83b93a133ef56a11dcb371ccff645824a7db062b
boog1108 <boogyjam@gmail.com>
2026-03-26 15:46:38 +0100

    added db population at start

--- src/main/java/dev/boog/money_tracker_auth/MoneyTrackerAuthApplication.java
+++ src/main/java/dev/boog/money_tracker_auth/MoneyTrackerAuthApplication.java
@@ -1,7 +1,16 @@
 package dev.boog.money_tracker_auth;
 
+import dev.boog.money_tracker_auth.entities.User;
+import dev.boog.money_tracker_auth.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class MoneyTrackerAuthApplication {
@@ -10,4 +19,22 @@ public class MoneyTrackerAuthApplication {
         SpringApplication.run(MoneyTrackerAuthApplication.class, args);
     }
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            List<User> userList = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                User user = User.builder()
+                        .email("user" + (i+1) + "@gmail.com")
+                        .password("password")
+                        .build();
+                userList.add(user);
+            }
+
+            userRepository.saveAll(userList);
+        };
+    }
 }
