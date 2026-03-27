29b6259d60b17319c03dfc17bedcb29166f33c89
C331732 <zieaboubacar.bamba@unicredit.eu>
2026-03-27 16:46:59 +0100

    added user role

--- src/main/java/dev/boog/money_tracker_auth/config/SecurityConfiguration.java
+++ src/main/java/dev/boog/money_tracker_auth/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package dev.boog.money_tracker_auth.config;
 
-import dev.boog.money_tracker_auth.config.filters.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -27,20 +26,27 @@ public class SecurityConfiguration {
                         .permitAll())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**")
-                        .authenticated())
+                        .authenticated()
+                )
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.builder()
+        UserDetails admin = User.builder()
                 .username("admin")
                 .password(passwordEncoder().encode("admin"))
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(userDetails);
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
     @Bean
